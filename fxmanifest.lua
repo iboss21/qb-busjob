a1adc122a679869cf1c3c0cdb936af62c88d9509
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-03-22 17:35:40 -0600

    Framework Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,14 +4,29 @@ game 'gta5'
 description 'QB-BusJob'
 version '1.0.0'
 
-shared_scripts {
-    '@qb-core/shared/locale.lua',
-	'locales/en.lua',
-    'config.lua'
+ui_page 'html/ui.html'
+
+client_scripts {
+	"client/main.lua",
+	"client/ui.lua"
 }
 
-client_script 'client/main.lua'
+server_scripts {
+	"main.lua",
+}
 
-server_script 'server/main.lua'
-
-lua54 'yes'
+files {
+    'html/css/bootstrap.min.css',
+    'html/css/bootstrap-switch.css',
+    'html/css/main.css',
+	'html/css/style.css',
+    'html/css/reset.css',	
+	'html/ui.html',
+    'html/ontvang.js',  
+	'html/res/futurastd-medium.css',
+    'html/res/futurastd-medium.eot',
+    'html/res/futurastd-medium.woff',
+    'html/res/futurastd-medium.ttf',
+    'html/res/futurastd-medium.svg',
+	'html/img/BUS_UI.png',
+}
